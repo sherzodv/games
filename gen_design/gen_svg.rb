080906f2a79dbae32aff4d7fe363ac59d86bde78
oybek <aybek_hashimov@mail.ru>
2014-10-10 04:34:02 +0600

    Generating hex with rb

--- gen_design/gen_svg.rb
+++ gen_design/gen_svg.rb
@@ -20,8 +20,7 @@ file = File.open(filename, "w");
 file.puts <<ENDSVG
 <?xml version="1.0" standalone="no"?>
 <svg width="256" height="256" version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <polygon points="#{a.join(" ")}"
-	  stroke="green" fill="transparent" stroke-width="5"/>
+  <polygon points="#{a.join(" ")}" style="fill:grey" fill="transparent" stroke-width="0"/>
 </svg>
 ENDSVG
 
