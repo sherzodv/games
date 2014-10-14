27ff7ab0a185679fd596167e9be8c1d1e53f8d4c
oybek <aybek_hashimov@mail.ru>
2014-10-14 20:42:53 +0600

    Generating field with program gen_svg.rb

--- gen_design/gen_svg.rb
+++ gen_design/gen_svg.rb
@@ -1,26 +1,51 @@
 
-s = nil
-s = gets
+str = gets
 
-rO = s.split(' ')[0].to_i;
-filename = s.split(' ')[1];
+$screen_width = str.split(';')[0].to_i
+$screen_height = str.split(';')[1].to_i
+$c = str.split(';')[2]
+$n = str.split(';')[3].to_i
+filename = str.split(';')[4].strip
 
-ro = 0.87 * rO;
+$s = 1
 
-a = [
-	[ro, 0],
-	[2*ro, rO/2],
-	[2*ro, 3*rO/2],
-	[ro, 2*rO],
-	[0, 3*rO/2],
-	[0, rO/2],
-]
+case $c
+when 'v'
+	$r = ($screen_height - $s * ($n + 1))/($n * 1.73)
+when 'h'
+	$r = (2 * $screen_width - $s * ($n + 1) * 1.73) / (4 + 3 * $n);
+end
+
+$h = 0.87 * $r
+
+def draw_hex(h, r, x, y)
+	v = [
+		[x, y - r],
+		[x + h, y - r/2],
+		[x + h, y + r/2],
+		[x, y+r],
+		[x - h, y + r/2],
+		[x - h, y - r/2]
+	]
+end
 
 file = File.open(filename, "w");
-file.puts <<ENDSVG
-<?xml version="1.0" standalone="no"?>
-<svg width="256" height="256" version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <polygon points="#{a.join(" ")}" style="fill:grey" fill="transparent" stroke-width="0"/>
-</svg>
-ENDSVG
+
+file.puts '<?xml version="1.0" standalone="no"?>'
+file.puts "<svg width=\"#{$screen_width}\" height=\"#{$screen_height}\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"
+
+	shift = false
+	$y = $s + $r
+	while $y < $screen_height
+		$x = $s + (shift ? 2*$h + $s/2 : $h);
+		shift = !shift;
+		while $x < $screen_width do
+			file.print "<polygon points=\"#{draw_hex($h, $r, $x, $y).join(" ")}\" style=\"fill:grey\" fill=\"transparent\" stOoke-width=\"0\"/>"
+
+			$x += $s + 2 * $h
+		end
+		$y += $s + 1.5*$r
+	end
+
+file.puts '</svg>'
 
