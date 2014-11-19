7104612fc30a4c1018e16792b5b5ab560979ae53
oybek <aybek_hashimov@mail.ru>
2014-11-19 22:27:24 +0600

    wtf view

--- naja/android/src/com/umniks/game/naja/android/AndroidLauncher.java
+++ naja/android/src/com/umniks/game/naja/android/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.umniks.game.naja.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -10,7 +12,20 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		RelativeLayout layout = new RelativeLayout(this);
+
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Naja(), config);
+		View gameView = initializeForView(new Naja(), config);
+
+		layout.addView(gameView);
+
+		setContentView(layout);
 	}
 }
+
