9149a0d8f1870a2a0c470c5be1b1ccdc181e9d7b
oybek <aybek_hashimov@mail.ru>
2014-11-22 19:23:32 +0600

    Adding advetising bar

--- naja/android/src/com/umniks/game/naja/android/AndroidLauncher.java
+++ naja/android/src/com/umniks/game/naja/android/AndroidLauncher.java
@@ -1,12 +1,26 @@
 package com.umniks.game.naja.android;
 
+import com.umniks.game.naja.Naja;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.umniks.game.naja.Naja;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -15,17 +29,36 @@ public class AndroidLauncher extends AndroidApplication {
 
 		RelativeLayout layout = new RelativeLayout(this);
 
-		//requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+	   // Create the layout
+        RelativeLayout layout = new RelativeLayout(this);
 
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		View gameView = initializeForView(new Naja(), config);
+        // Do the stuff that initialize() would do for you
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
-		layout.addView(gameView);
+        // Create the libgdx View
+        View gameView = initializeForView(new HelloWorld(), false);
 
-		setContentView(layout);
+        // Create and setup the AdMob view
+        //AdView adView = new AdView(this, AdSize.BANNER, "xxxxxxxx"); // Put in your secret key here
+        adView.loadAd(new AdRequest());
+
+        // Add the libgdx view
+        layout.addView(gameView);
+
+        // Add the AdMob view
+        RelativeLayout.LayoutParams adParams =
+            new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+        adParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        layout.addView(adView, adParams);
+
+        // Hook it all up
+        setContentView(layout);
 	}
 }
 
